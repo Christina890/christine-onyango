9c563523b7567e945dedd3edc8afc4473338c575
Christine Apondi Onyango <christine.onyango@Admins-MacBook-Pro.local>
2022-05-29 11:09:17 +0300

    request specification, addpet, getpet

---
+++ appium-tests/src/test/java/TestCases.java
@@ -0,0 +1,54 @@
+import groovyjarjarpicocli.CommandLine;
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+
+public class TestCases {
+    RequestSpecification requestSpec;
+    @BeforeClass
+    public void specs(){
+        requestSpec=given().baseUri("https://petstore3.swagger.io/api/v3/pet").contentType("application/json");
+    }
+
+    @Test
+    void addPet(){
+        Map<String, Object> pet = new LinkedHashMap<>();
+        pet.put("id", "23");
+        pet.put("name", "Scooby");
+        Map<String, String> categoryMap = new LinkedHashMap<>();
+        categoryMap.put("id", "2");
+        categoryMap.put("name", "dogs");
+        pet.put("category",categoryMap);
+        String[] photolist = {"url"};
+        pet.put("photoUrls",photolist);
+        pet.put("status","available");
+        //Map<String, String> tags = new LinkedHashMap<>();
+        //tags.put("id","0");
+        //tags.put("name","doggie");
+        //pet.put("tags",tags);
+        pet.put("status","available");
+        Response response = given().spec(requestSpec).when().body(pet).post();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
+    @Test
+    void getPet(){
+        Response response = given().spec(requestSpec).basePath("/findByStatus").queryParam("status", "available").when().get();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+}
+
+}
