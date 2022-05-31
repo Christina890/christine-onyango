f2048e1a1de7e51b575e0185f1a6b1b190cd91da
Christine Apondi Onyango <christine.onyango@Admins-MacBook-Pro.local>
2022-05-31 22:16:22 +0300

    Add income and expense tests

---
+++ monefy-android-tests/src/test/java/tests/TestCases.java
@@ -0,0 +1,89 @@
+package tests;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+public class TestCases {
+    public AndroidDriver<MobileElement> driver;
+    public WebDriverWait                wait;
+
+    By continueButton = By.id("com.monefy.app.lite:id/buttonContinue");
+    By closeButton = By.id("com.monefy.app.lite:id/buttonClose");
+    By addExpense   = By.id("com.monefy.app.lite:id/expense_button_title");
+    By keyboard1 =   By.id("com.monefy.app.lite:id/buttonKeyboard1");
+    By keyboard0 =  By.id("com.monefy.app.lite:id/buttonKeyboard0");
+    By chooseCategory = By.id("com.monefy.app.lite:id/keyboard_action_button");
+    By category = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.GridView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.ImageView");
+    By getBalance = By.id("com.monefy.app.lite:id/balance_amount");
+    By addIncome = By.id("com.monefy.app.lite:id/income_button");
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "MyAndroid");
+        caps.setCapability("udid", "emulator-5554");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "11.0");
+        caps.setCapability("skipUnlock", "true");
+        caps.setCapability("appPackage", "com.monefy.app.lite");
+        caps.setCapability("appActivity", "com.monefy.activities.main.MainActivity_");
+        caps.setCapability("noReset", "false");
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    void addExpense(){
+        //Click through the splash screens
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+
+        //Close the promo
+        wait.until(ExpectedConditions.visibilityOfElementLocated(closeButton)).click();
+
+        //Click to add expense
+        wait.until(ExpectedConditions.visibilityOfElementLocated(addExpense)).click();
+
+        //Enter Amount
+        wait.until(ExpectedConditions.visibilityOfElementLocated(keyboard1)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(keyboard0)).click();
+
+        //Click to choose category
+        wait.until(ExpectedConditions.visibilityOfElementLocated(chooseCategory)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(category)).click();
+        String balance =wait.until(ExpectedConditions.visibilityOfElementLocated(getBalance)).getText();
+        Assert.assertEquals(balance, "Balance -$10.00");
+    }
+    @Test
+    void addIncome(){
+        //Click through the splash screens
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(continueButton)).click();
+
+        //Close the promo
+        wait.until(ExpectedConditions.visibilityOfElementLocated(closeButton)).click();
+
+        //Click to add expense
+        wait.until(ExpectedConditions.visibilityOfElementLocated(addIncome)).click();
+
+        //Enter Amount
+        wait.until(ExpectedConditions.visibilityOfElementLocated(keyboard1)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(keyboard0)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(keyboard0)).click();
+
+        //Click to choose category
+        wait.until(ExpectedConditions.visibilityOfElementLocated(chooseCategory)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(category)).click();
+        String balance =wait.until(ExpectedConditions.visibilityOfElementLocated(getBalance)).getText();
+        Assert.assertEquals(balance, "Balance $100.00");
+    }
+}
+
